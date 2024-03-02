7f1c72f49499115da1e8b60425b7f61bea5b24cf
Rabby <67389402+RabbyDevs@users.noreply.github.com>
2024-03-02 15:36:36 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,31 +1,60 @@
 const rbxlx = require("@shiinazzz/rbxm-reader");
-console.log(rbxlx.parseBuffer())
-const util = require("util");
-const path = require("path");
+const express = require('express');
+const app = express ();
+app.use(express.json());
+const PORT = process.env.PORT || 2530;
+console.log(PORT)
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+  });
+const axios = require('axios');
+const crypto = require("crypto");
 const fs = require("fs");
+const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
 
-const path = "./72109a3cc0927472b41523bb75b6dcfb.rbxlx";
-const file = Bun.file(path);
+async function asset(assetId) {
+    const response = await axios.get(`https://assetdelivery.roblox.com/v1/asset?id=${assetId}`, { responseType: 'arraybuffer' });
+    const fileHash = crypto.randomUUID()
+    fs.writeFileSync(`./cache/${fileHash}`, response.data)
+    return `./cache/${fileHash}`
+}
+async function getImageId(assetId) {
+    const assetFilePath = await asset(assetId)
+    try {
+        const obtainedData = rbxlx.parseBuffer(fs.readFileSync(assetFilePath))
+        
+        for (const instance of obtainedData.instances) {
+            if (instance.TextureID !== undefined) {
+                let finalId = instance.TextureId
+                finalId.replace('http://www.roblox.com/asset/?id=', '')
+                finalId.replace('rbxassetid://', '')
+                return [finalId, assetFilePath]
+            }
+            if (instance.TextureId !== undefined) {
+                let finalId = instance.TextureId
+                finalId = finalId.replace('http://www.roblox.com/asset/?id=', '')
+                finalId = finalId.replace('rbxassetid://', '')
+                return [finalId, assetFilePath]
+            }
+        }
+    }
+    catch {
+        try {
+            const parser = new XMLParser();
+            let xml = parser.parse(fs.readFileSync(assetFilePath));
+            let finalId = xml.roblox.Item.Properties.Content.url
+            finalId = finalId.replace('http://www.roblox.com/asset/?id=', '')
+            finalId = finalId.replace('rbxassetid://', '')
+            return [finalId, assetFilePath]
+        }
+        catch {
+            return ['', assetFilePath]
+        }
+    }
+}
 
-arrayBuffer(fs.readfileas)
-
-const buffer = await file.arrayBuffer();
-
-rbxlx.parseBuffer()
-rbxlx.parseBuffer(fs.readFileSync(path.join(__dirname, "")))
-.then(tree => {
-    // console.log("Parsed file!");
-
-    // var descendants = tree.getDescendants();
-    // for (var object of descendants) {
-    //     if (object.class == "Part") {
-    //         console.log("We found a Part named", object.getProperty("Name"));
-    //     }
-    // }
-
-    // You can also output the entire tree structure
-    console.log(util.inspect(tree, {depth: Infinity, colors: true}));
-})
-.catch(err => {
-    console.log(`Could not parse file because: ${err.message}`);
-})
+app.get("/image", async(request, response) => {  
+    const imageIdData = await getImageId(request.query.id);
+    fs.unlinkSync(imageIdData[1])
+    response.send(imageIdData[0]);
+ });
